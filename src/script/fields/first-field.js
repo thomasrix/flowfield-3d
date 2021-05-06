c22ae490b8fbc37773a99f058f427f56cebbc1e4
Thomas Rix <trix@dr.dk>
2021-05-06 12:59:04 +0200

    Adding objects and OrbitControl

--- src/script/fields/first-field.js
+++ src/script/fields/first-field.js
@@ -2,6 +2,7 @@
 import '../../style/canvas.scss';
 import {create, select} from '../utils/trix';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 export default class FirstField{
     constructor(){
@@ -12,11 +13,11 @@ export default class FirstField{
     }
     build(){
         const container = create('div', select('body'), 'canvas-container');
-        const canvas = create('canvas', container, 'draw-canvas');
-        canvas.width = this.width;
-        canvas.height = this.height;
+        this.canvas = create('canvas', container, 'draw-canvas');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
 
-        this.ctx = canvas.getContext('webgl');
+        this.ctx = this.canvas.getContext('webgl');
         this.buildScene();
     }
     buildScene(){
@@ -25,15 +26,91 @@ export default class FirstField{
         this.renderer = new THREE.WebGLRenderer({
             context:this.ctx
         });
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        console.log(this.renderer)
         this.renderer.setSize(this.width, this.height);
 
+        
+        this.addCamera();
+        this.addLights();
+        this.addPlane();
+        this.addSphere();
+
+        this.controls = new OrbitControls( this.camera, this.canvas );
+        this.controls.update();
+        console.log(this.renderer.domElement);
+        // this.controls.autoRotate = true;
+        this.renderer.render(this.scene, this.camera); 
+        this.boundAni = this.animate.bind(this);
+        this.boundAni();
+        
+    }
+    animate(){
+        // console.log('animate');
+        requestAnimationFrame(this.boundAni);
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera); 
+    }
+    addCamera(){
         this.camera = new THREE.PerspectiveCamera(40, this.width / this.height, 0.1, 1000);
         this.camera.position.z = 12;
         this.camera.position.x = 0;
         this.camera.position.y = 5;
+    
+        // this.camera.lookAt(0, 0, 0);
         
         this.scene.add(this.camera);
+        
+    }
+    addLights(){
+        this.scene.add(new THREE.AmbientLight('hsl(0, 0%, 90%)'));
+        let d_light = new THREE.PointLight( 0xffffcc, 1, 0, 2);
+        
+        d_light.castShadow = true;
+        d_light.shadow.mapSize.width = 2048; // default
+        d_light.shadow.mapSize.height = 2048; // default
+        d_light.shadow.camera.near = 0.5; // default
+        d_light.shadow.camera.far = 500; // default
 
-        this.renderer.render(this.scene, this.camera); 
+
+        d_light.position.set( 10, 15, 7 );
+        
+        this.scene.add(d_light);
+
+    }
+    addPlane(){
+        this.planeGeom = new THREE.PlaneGeometry(5, 5, 10, 10);
+
+        const material = new THREE.MeshStandardMaterial({
+            color: '#666',
+            metalness:0,
+            roughness:0.1,
+            wireframe:false,
+            side:THREE.DoubleSide
+        });
+
+        this.plane = new THREE.Mesh(this.planeGeom, material);
+        this.plane.rotateX(-Math.PI * 0.5);
+        this.plane.receiveShadow = true;
+        this.scene.add(this.plane);
+
+        console.log(this.plane.position)
+    }
+    addSphere(){
+        this.sphereGeom = new THREE.SphereGeometry( 0.25, 32, 32 );
+
+        const material = new THREE.MeshStandardMaterial({
+            color: '#ff0000',
+            metalness:0.5,
+            roughness:0.1,
+            wireframe:false
+        });
+
+        const mesh = new THREE.Mesh(this.sphereGeom, material);
+        mesh.position.y = 0.75;
+        mesh.castShadow = true;
+        this.scene.add(mesh);
+
     }
 }
--- src/script/index.js
+++ src/script/index.js
@@ -2,7 +2,7 @@ import BlurMotion from './fields/blur-motion';
 import FirstField from './fields/first-field';
 import {create, select} from './utils/trix';
 console.log('Yo!');
-// const f = new FirstField();
-const f = new BlurMotion();
+const f = new FirstField();
+// const f = new BlurMotion();
 
 
