2c1ad1fc8d135e37ccdcc304d6cfe5b2236ef342
Thomas Rix <trix@dr.dk>
2021-05-10 09:17:13 +0200

    Added pointer to flow markers

--- src/script/fields/flow.js
+++ src/script/fields/flow.js
@@ -1,5 +1,6 @@
 'use strict'
 import * as THREE from 'three';
+import { NumberKeyframeTrack } from 'three';
 
 export default class Flow{
     constructor(scene, resolution){
@@ -11,7 +12,7 @@ export default class Flow{
         console.log('build flow');
         this.initSpheres();
         // this.makeSphere(0, 0.05, 0);
-        this.createCubeGrid(10, 10);
+        this.createCubeGrid(10, 5);
         
     }
     createCubeGrid(size, count){
@@ -27,7 +28,7 @@ export default class Flow{
                 for(let k = 0 ; k < count ; k++){
                     dy = startY + (step * k);
                     // console.log(dz, dx, dy);
-                    this.makeSphere(dx, dy, dz);
+                    this.makeMarker(dx, dy, dz);
                 }
             }
         }
@@ -36,11 +37,31 @@ export default class Flow{
         this.sphereGeom = new THREE.SphereGeometry( 0.05, 16, 16 );
     
         this.material = new THREE.MeshStandardMaterial({
-            color: '#ff0000',
+            color: '#999900',
             metalness:0.5,
             // roughness:0.1,
-            wireframe:false
+            wireframe:false,
+            side:THREE.DoubleSide
         });
+    }
+    makeMarker(x = 0, y = 0, z = 0){
+        this.makeSphere(x, y, z);
+        this.makePointer(x, y, z);
+    }
+    makePointer(x = 0, y = 0, z = 0){
+        const pull = this.getValue(x, y, z);
+        console.log(x, pull.x);
+        const points = [
+            new THREE.Vector3(x, y, z),
+            new THREE.Vector3(pull.x, pull.y, pull.z),
+        ]
+        // const geometry = new THREE.BufferGeometry().setFromPoints( points );
+        const line = new THREE.LineCurve3(new THREE.Vector3(x, y, z), new THREE.Vector3(pull.x, pull.y, pull.z));
+        const tubeGeom = new THREE.TubeGeometry( line, 64, 0.01, 8, false );
+        const mesh = new THREE.Mesh( tubeGeom, this.material );
+        // const line = new THREE.Line( geometry );
+        this.scene.add( mesh );
+
     }
     makeSphere(x = 0, y = 0, z = 0){
 
@@ -48,5 +69,15 @@ export default class Flow{
         mesh.position.set(x, y, z);
         mesh.castShadow = true;
         this.scene.add(mesh);
+    }
+    getValue(x = 0, y = 0, z = 0){
+        let nx = x + 0.2;
+        let ny = y + 0.2;
+        let nz = z + 0;
+        return {
+            x:nx,
+            y:ny,
+            z:nz
+        }
     }
 }
